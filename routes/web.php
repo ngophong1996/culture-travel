13c83ccca30930c4a9f22e75b7f417a80cb95b18
ngophong1996 <ngophong1996@gmail.com>
2022-09-01 13:51:59 +0900

    changeVietnamPage

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vietnam',function(){
+    return view(('vietnam.vietnam'));
+});
